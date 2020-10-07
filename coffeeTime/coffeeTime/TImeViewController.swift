786354a2d40673192392b36e392be795beef4c87
Keita Suzuki <>
2020-10-07 12:55:16 +0900

    タブバー追加

---
+++ coffeeTime/coffeeTime/TImeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TImeViewController.swift
+//  coffeeTime
+//
+//  Created by Keita Suzuki on 2020/10/07.
+//  Copyright © 2020 Keita Suzuki. All rights reserved.
+//
+
+import UIKit
+
+class TImeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
